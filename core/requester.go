08c975f4486a6a94741a1e6e87ccd4847cb875be
viettranx <viettranx@gmail.com>
2023-03-19 21:44:48 +0700

    Add new GetRequester function

--- core/requester.go
+++ core/requester.go
@@ -1,23 +1,31 @@
 package core
 
+import "context"
+
+const KeyRequester = "requester"
+
 type TokenPayload struct {
-	UId   int    `json:"user_id"`
-	URole string `json:"role"`
+	UId int `json:"user_id"`
 }
 
 func (p TokenPayload) UserId() int {
 	return p.UId
 }
 
-func (p TokenPayload) Role() string {
-	return p.URole
-}
-
 type Requester interface {
 	GetUserId() int
 	GetRole() string
+	GetStatus() string
 }
 
 func IsAdmin(requester Requester) bool {
 	return requester.GetRole() == "admin" || requester.GetRole() == "mod"
+}
+
+func GetRequester(ctx context.Context) Requester {
+	if requester, ok := ctx.Value(KeyRequester).(Requester); ok {
+		return requester
+	}
+
+	return nil
 }
