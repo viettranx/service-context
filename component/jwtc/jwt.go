d98c32484881d8d72feda59a2528f50a7c31f362
viettranx <viettranx@gmail.com>
2023-03-20 18:36:58 +0700

    Add JWT component

---
+++ component/jwtc/jwt.go
@@ -0,0 +1,107 @@
+package jwtc
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/pkg/errors"
+	sctx "github.com/viettranx/service-context"
+	"time"
+)
+
+const (
+	defaultSecret               = "very-important-please-change-it!" // in 32 bytes
+	defaultExpireTokenInSeconds = 60 * 60 * 24 * 7                   // 7d
+)
+
+var (
+	ErrSecretKeyNotValid     = errors.New("secret key must be in 32 bytes")
+	ErrTokenLifeTimeTooShort = errors.New("token life time too short")
+)
+
+type jwtx struct {
+	id                   string
+	secret               string
+	expireTokenInSeconds int
+}
+
+func NewJWT(id string) *jwtx {
+	return &jwtx{id: id}
+}
+
+func (j *jwtx) ID() string {
+	return j.id
+}
+
+func (j *jwtx) InitFlags() {
+	flag.StringVar(
+		&j.secret,
+		"jwt-secret",
+		defaultSecret,
+		"Secret key to sign JWT",
+	)
+
+	flag.IntVar(
+		&j.expireTokenInSeconds,
+		"jwt-exp-secs",
+		defaultExpireTokenInSeconds,
+		"Number of seconds token will expired",
+	)
+}
+
+func (j *jwtx) Activate(_ sctx.ServiceContext) error {
+	if len(j.secret) < 32 {
+		return errors.WithStack(ErrSecretKeyNotValid)
+	}
+
+	if j.expireTokenInSeconds <= 60 {
+		return errors.WithStack(ErrTokenLifeTimeTooShort)
+	}
+
+	return nil
+}
+
+func (j *jwtx) Stop() error {
+	return nil
+}
+
+func (j *jwtx) IssueToken(ctx context.Context, id, sub string) (token string, expSecs int, err error) {
+	now := time.Now().UTC()
+
+	claims := jwt.RegisteredClaims{
+		Subject:   sub,
+		ExpiresAt: jwt.NewNumericDate(now.Add(time.Second * time.Duration(j.expireTokenInSeconds))),
+		NotBefore: jwt.NewNumericDate(now),
+		IssuedAt:  jwt.NewNumericDate(now),
+		ID:        id,
+	}
+
+	t := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	tokenSignedStr, err := t.SignedString([]byte(j.secret))
+
+	if err != nil {
+		return "", 0, errors.WithStack(err)
+	}
+
+	return tokenSignedStr, j.expireTokenInSeconds, nil
+}
+
+func (j *jwtx) ParseToken(ctx context.Context, tokenString string) (id string, sub string, err error) {
+	var claims jwt.RegisteredClaims
+
+	token, err := jwt.ParseWithClaims(tokenString, &claims, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+
+		return []byte(j.secret), nil
+	})
+
+	if !token.Valid {
+		return "", "", errors.WithStack(err)
+	}
+
+	return claims.ID, claims.Subject, nil
+}
--- go.mod
+++ go.mod
@@ -28,6 +28,7 @@ require (
 	github.com/go-playground/validator/v10 v10.11.2 // indirect
 	github.com/go-sql-driver/mysql v1.7.0 // indirect
 	github.com/goccy/go-json v0.10.0 // indirect
+	github.com/golang-jwt/jwt/v5 v5.0.0-rc.1 // indirect
 	github.com/golang-sql/civil v0.0.0-20220223132316-b832511892a9 // indirect
 	github.com/golang-sql/sqlexp v0.1.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
